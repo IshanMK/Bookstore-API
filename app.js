eec5c01970b3f7488d476075732070a2203e8bfc
IshanMK <e18173@eng.pdn.ac.lk>
2024-08-30 03:54:30 +0530

    Final update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const Book = require("./models");
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
-app.use(express.json());
+// app.use(express.json());
+app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose.connect("mongodb://mongodb:27017/bookstore", {
